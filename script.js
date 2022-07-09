587f8e539b28d7e887b4c8089b078d18b7d2ed7b
cliftonchee <70459953+cliftonchee@users.noreply.github.com>
2022-07-10 01:02:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+/* Nav bar scrolling */
+function scrollToElderly() {
+    document.querySelector(".elderly_marking").scrollIntoView()
+}
+
+function scrollToChildren() {
+    document.querySelector(".children_marking").scrollIntoView()
+}
+
+function scrollToHandicap() {
+    document.querySelector(".handicap_marking").scrollIntoView()
+}
+
+function scrollToEnvironment() {
+    document.querySelector(".environment_marking").scrollIntoView()
+}
+
+function scrollToPet() {
+    document.querySelector(".animal_marking").scrollIntoView()
+}
+
